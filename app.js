46c93a3dbba0fc22ce1a9e3b17447c0facf9043b
pauladonis <pauladonismacavei@gmail.com>
2022-04-01 09:37:43 +0100

    dynamically setting port to fix heroku error

--- app.js
+++ app.js
@@ -346,6 +346,6 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.listen(process.env.port, () => {
+  console.log(`Example app listening at http://localhost:${process.env.port}`)
 });
